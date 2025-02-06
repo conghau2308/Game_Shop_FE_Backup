7e5f17df54b2461c7738093721760280b2b1c7a7
conghau2308 <vooconghau2604@gmail.com>
2025-02-06 19:18:53 +0700

    conghau/LoginPage

--- A4Team/src/main.jsx
+++ A4Team/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import LoginPage from './pages/Customer/LoginPage/loginpage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
